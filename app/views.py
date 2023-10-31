935aedc9f5540ef02a8c4e0f0831b719ef6dde2b
Лёва Нерсесян <lyova123454321@gmail.com>
2023-10-31 19:56:13 +0300

    update

--- app/views.py
+++ app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
 
 # Create your views here.
+QUESTION = [
+        {
+            'id': i,
+            'title': f'Question {i+1}',
+            'content': f'long lorem ipsum {i+1}'
+        } for i in range(100)
+    ]
+
+
+def paginate(objects, page, per_page=10):
+    paginator = Paginator(objects, per_page)
+    return paginator.page(page)
+
+
+def index(request):
+    page = int(request.GET.get('page', 1))
+    return render(request, 'index.html', {'questions': paginate(QUESTION, page)})
+
+
+def question(request, question_id):
+    item = QUESTION[question_id]
+    return render(request, 'question.html', {'question': item})
